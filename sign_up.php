faf0de438e835468303383404c82690fcee498b5
Nick Mathews <nickinnik@gmail.com>
2015-05-25 16:52:48 -0700

    #4: @tflorida17, I checked this and the USER, INSTITUITION_MEMBER, and CREDENTIALS tables fill properly when I trigger sign_up.js.  The confirmed column changes from 0 to 1 when I click the confirmation link.  Approved

--- sign_up.php
+++ sign_up.php
@@ -3,30 +3,63 @@
 	<title>Sign Up</title>
 	<script type="text/javascript" src="scripts/sign_up.js"></script>
 	<meta name="description" content="Allows guest to sign up for a basic account">
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/selectors.css">
+    <link rel="stylesheet" href="css/responsive.css">
+	
 </head>
 <body>
-<form action="">
+<div id="signup-wrapper">
+<form class="signup-form" action="">
 	<h1>Sign Up</h1>
-	<br>
-	First Name: <input name="First Name" id="first_name" type="text" value="">
-	<br>
-	Last Name: <input name="Last Name" id="last_name" type="text" value="">
-	<br>
-	Date of Birth: <input type="date" id="date_birth" name="date" value="">
-	<br>
-	Gender: <select name="Gender" id="gender_id">
-		<option value="-1">Select Your gender</option>
-		<option value="0">Male</option>
-		<option value="1">Female</option>
-	</select>
-	<br>
-	Email: <input type="email" id="email" name="email" value="">
-	<br>
-	Password: <input type="password" id="password" name="password" value="">
-	<br>
+
+	<fieldset>
+
+	<legend><span class="number">1</span> Your basic info</legend>
+
+	
+		<label for="first_name">First Name:</label> 	
+		<input name="First Name" id="first_name" type="text" value="">
+		
+		<label for="last_name">Last Name:</label>
+		<input name="Last Name" id="last_name" type="text" value="">
+		
+		<label for="date">Date of Birth:</label> 
+		<input type="date" id="date_birth" name="date" value="">
+		
+		<label for="gender_id">Select your gender:</label>
+			<input type="radio" name="gender_id" id="gender_id" value="0"<label>Male</label><br>
+			<input type="radio" name="gender_id" id="gender_id" value="1"<label>Female</label><br>			
+<!--		    <option value="-1">Select Your gender</option> 
+				<option value="0">Male</option>
+				<option value="1">Female</option>  
+-->
+	</fieldset>
+	
+	<fieldset>
+	
+	<legend><span class="number">2</span> Create your account</legend>
+		<p>If you wish to have your own profile page, you must use your institution provided email.</p> 
+		<p> Example: yourname@institution.edu</p>
+		<label for="email">Email:</label>
+		<input type="email" id="email" name="email" value="">
+		
+		<label for="password">Password:</label> 
+		<input type="password" id="password" name="password" value="">
+	
+	</fieldset>
+
+
+	<legend><span class="number">3</span> You're so close!</legend><br>
+	
 </form>
+
 <button onclick="sign_up()" id="sign_up">Sign Up</button>
+
 <p id="status"></p>
+
+</div>
 </body>
 </html>
 
