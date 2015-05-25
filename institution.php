3e01ee37642e47da1bb8f61a50bc20f58e499e61
Nick Mathews <nickinnik@gmail.com>
2015-05-25 15:06:16 -0700

    #3: @tflorida17 No real changes, I only spruced up the table a bit.  Also I took out the lat and lon from the table.  We should look into a Google Maps API plugin or something at some point

--- institution.php
+++ institution.php
@@ -6,15 +6,15 @@
 	<meta charset="utf-8">
     <link rel="stylesheet" href="css/normalize.css">
     <link href='http://fonts.googleapis.com/css?family=Changa+One|Open+Sans:400italic,700italic,400,700,800' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="css/responsive.css">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/responsive.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 <?php
-
 include "helpers/header.html";
 include "scripts/institution.php"; 
+
 // Get institution data from the above include
 $name=$inst[0];
 $link_website=$inst[1];
@@ -25,20 +25,100 @@ $state_abbrev=$inst[5];
 $zip_code=$inst[6];
 $city=$inst[7];
 
+	
+//////////////////////////////
+///////   CONTENT    /////////
+//////////////////////////////
+echo '<div id="wrapper">';
 
-// Display the institution data on the page
 
-echo '<h3>'.$name.'</h3>';
-echo '<a href="http://'.$link_website.'">Official Website</a>';
-echo '<table border=1><tr><td>Lat</td><td>'.$latitude.'</td></tr>';
-echo '<tr><td>Lon</td><td>'.$longitude.'</td></tr></table>';
-echo '<table><tr><td>'.$street_address.'</td></tr>';
-echo '<tr><td>'.$city.', '.$state_abbrev.' '.$zip_code.'</td></tr></table>';
+///////   SIDE-BAR   /////////
+
+
+echo '
+<div class="left-bar">
+    <table>
+      <caption>Institution Details</caption>
+      <thead>
+        <tr>
+          <th scope="col" colspan="2">'.$name.'</th>
+        </tr>
+      </thead>
+      <tfoot>
+        <tr>
+          <td colspan="3">Data last updated on INSERT DATE HERE</td>
+        </tr>
+      </tfoot>
+      <tbody>
+        <tr>
+          <th scope="row">Official Website:</th>
+          <td><a href="http://'.$link_website.'">'.$link_website.'</a></td>
+        </tr>
+        <tr>
+          <th scope="row">City:</th>
+          <td>'.$city.'</td>
+        </tr>
+        <tr>
+          <th scope="row">State:</th>
+          <td>'.$state_abbrev.'</td>
+        </tr>
+        <tr>
+          <th scope="row">Address:</th>
+          <td>'.$street_address.'</td>
+        </tr>
+        <tr>
+          <th scope="row">Zip Code:</th>
+          <td>'.$zip_code.'</td>
+        </tr>		
+      </tbody>
+    </table>
+</div>
+
+
+
+<div class="right-bar">
+    <table>
+      <caption>Institution Details</caption>
+      <thead>
+        <tr>
+          <th scope="col" colspan="2">'.$name.'</th>
+        </tr>
+      </thead>
+      <tfoot>
+        <tr>
+          <td colspan="3">Data last updated on INSERT DATE HERE</td>
+        </tr>
+      </tfoot>
+      <tbody>
+        <tr>
+          <th scope="row">Official Website:</th>
+          <td><a href="http://'.$link_website.'">'.$link_website.'</a></td>
+        </tr>
+        <tr>
+          <th scope="row">Andrew Chalkley</th>
+          <td>andrew@example.com</td>
+        </tr>
+        <tr>
+          <th scope="row">Dave McFarland</th>
+          <td>dave@example.com</td>
+        </tr>
+        <tr>
+          <th scope="row">Guil Hernandez</th>
+          <td>guil@example.com</td>
+        </tr>
+      </tbody>
+    </table>
+</div>
+';
+		
+
+///////   FOOTER   /////////
 
 include "helpers/footer.html";
 
-
+echo '</div>'; //close wrapper div
 ?>
+
 </body>
 </html>
 
