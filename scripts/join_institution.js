0fc625fd4df91280bb4017ea1aaf7455082fd299
Tony Florida <raddfood@gmail.com>
2015-06-03 16:27:40 -0700

    #30: Joining an institution is complete.  I will link to this page from the wizard for now.

--- scripts/join_institution.js
+++ scripts/join_institution.js
@@ -14,6 +14,23 @@ function join_institution(first_name, user_id) {
 	},
 	function (data) {
 		// Set the status text
-		$("#status").text(data);
+		var message;
+		if(data > 0) {
+			message = "We've sent you an email with a link to complete the process of joining this institution. " + data;
+		} else if(data == -2) {
+			message = "Email not available";
+		} else if(data == -4) {
+			message = "Missing information";
+		} else if(data == -7) {
+			message = "Activation code generation failed";
+		} else if(data == -10) {
+			message = "Not a valid institutional email";
+		} else if(data == -11) {
+			message = "Email does not match selected institution";
+		} else {
+			message = "Unknown error";
+		}
+
+		$("#status").text(message);
 	});
 }
